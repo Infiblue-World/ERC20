646c4365b260412c10aa76be3339a876c46e1a3c
InfiblueWorld <dev@infiblue.world>
2022-07-10 23:31:22 -0700

    update contract
    - Monie: mint to 5 wallets
    - Monie: check lock in transferFrom
    - Lock: delay_lock, allow changes within 48 hours
    - Lock: update release date to Nov 1 2022
    - upgrade ERC20 to V4.7.0

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -10,24 +10,32 @@ contract Lock {
         uint256 current_locked;
         uint256 released;
         uint256 claimed_month;
+        uint256 locked_time;
     }
 
     address public owner;
     address public locker;
+    address unlocker;
     //this declares a state variable that
     //stores a User type (Beneficiary)
     mapping(address => Beneficiary) public beneficiaries;
-    // release start time
-    uint256 public release_date;
+    // release starts on Nov 1st 2022
+    uint256 public release_date = 1667286000;
 
-    constructor(uint256 _timestamp) {
+    constructor() {
         owner = msg.sender;
-        release_date = _timestamp;
     }
 
     function assignLocker(address _locker) external {
         require(msg.sender == owner);
+        require(_locker != address(0));
         locker = _locker;
+    }
+
+    function assignUnlocker(address _unlocker) external {
+        require(msg.sender == owner);
+        require(_unlocker != address(0));
+        unlocker = _unlocker;
     }
 
     function lock(
@@ -39,18 +47,36 @@ contract Lock {
             msg.sender == locker,
             "You don't have access to this function."
         );
-        require(block.timestamp < release_date);
+        uint256 current_time = block.timestamp;
+        require(current_time < release_date);
         if (beneficiaries[_user].month == 0) {
             beneficiaries[_user].total_locked = _amount;
             beneficiaries[_user].current_locked = _amount;
             beneficiaries[_user].month = _month;
             beneficiaries[_user].released = 0;
             beneficiaries[_user].claimed_month = 0;
+            beneficiaries[_user].locked_time = current_time;
         } else {
             require(_month == beneficiaries[_user].month);
             beneficiaries[_user].total_locked += _amount;
             beneficiaries[_user].current_locked += _amount;
+            beneficiaries[_user].locked_time = current_time;
         }
+    }
+
+    function unlock(
+        address _user,
+        uint256 _amount,
+        uint256 _month
+    ) external {
+        require(msg.sender == unlocker);
+        require(
+            //only allow changes within 48 hours after lock
+            block.timestamp - beneficiaries[_user].locked_time <= 48 * 3600 &&
+                beneficiaries[_user].month == _month
+        );
+        beneficiaries[_user].total_locked -= _amount;
+        beneficiaries[_user].current_locked -= _amount;
     }
 
     function release() external {
--- contracts/Monie.sol
+++ contracts/Monie.sol
@@ -7,13 +7,28 @@ import "./Lock.sol";
 
 contract MonieToken is ERC20 {
     Lock public lock;
-    address[] public signers;
+    uint256 supply_mine = 12 * 10**26;
+    uint256 supply_com = 3.5 * 10**26;
+    uint256 supply_t = 3 * 10**26;
+    uint256 supply_f = 1 * 10**26;
+    uint256 supply_s = 5 * 10**25;
 
-    constructor(uint256 initialSupply, address _lock)
+    constructor(
+        address _wallet_mine,
+        address _wallet_com,
+        address _wallet_t,
+        address _wallet_f,
+        address _wallet_s,
+        address _lock
+        )
         ERC20("$INFIBLUE WORLD", "$MONIE")
     {
         lock = Lock(_lock);
-        _mint(msg.sender, initialSupply);
+        _mint(_wallet_mine, supply_mine);
+        _mint(_wallet_com, supply_com);
+        _mint(_wallet_t, supply_t);
+        _mint(_wallet_f, supply_f);
+        _mint(_wallet_s, supply_s);
     }
 
     function transfer(address to, uint256 amount)
@@ -28,6 +43,24 @@ contract MonieToken is ERC20 {
             require(balance - amount >= current_locked);
         }
         _transfer(sender, to, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        uint256 current_locked = lock.check_lock(from).current_locked;
+        // lock will end by Nov 1st 2025, 36 months from Nov 1st 2022
+        if (block.timestamp <= 1761980400 && current_locked > 0) {
+            uint256 balance = balanceOf(from);
+            require(balance - amount >= current_locked);
+        }
+
+        address spender = msg.sender;
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
         return true;
     }
 }
